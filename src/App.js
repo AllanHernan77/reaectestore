7496412a07a9087c46a1e6ca3d9a13faf1ae29ef
AllanHernan77 <Allan00hernan@gmail.com>
2021-12-01 00:56:34 -0500

    comments

--- src/App.js
+++ src/App.js
@@ -8,12 +8,16 @@ import axios from 'axios'
 
 class App extends React.Component {
 
+  //declaring states for products which is an array, which we will use to filter and map
+  // search word for filtering
+  // PRICE DOES NOT WORK 
   state = {
     products :[],
     searchWord: "",
     Price : 0
   }
 
+  //calling API
   componentDidMount() {
     axios.get(`https://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline`)
       .then(response => {
@@ -35,25 +39,26 @@ class App extends React.Component {
 
         
         <div className="App">
-          
+          {/* nav bar, could had broke this down to different components but forgot to at the end*/}
           <div className = "navbar">
             <div className = "navblocks">
               <img src ={logo} alt = "logo" className =  "logo"></img>
-              <a href = "/#"> <h1 className = "navElement">Start</h1> </a>
-              <a href = "/#"><h1 className = "navElement">Sell</h1></a>
-              <a href = "/#"><h1 className = "navElement">Market</h1></a>
-              <a href = "/#"><h1 className = "navElement">Manage</h1></a>
+              <a href = "#"> <h1 className = "navElement">Start</h1> </a>
+              <a href = "#"><h1 className = "navElement">Sell</h1></a>
+              <a href = "#"><h1 className = "navElement">Market</h1></a>
+              <a href = "#"><h1 className = "navElement">Manage</h1></a>
             </div>
             <div className = "navblocks">
-            <a href = "/#"><h1 className = "navElement">Pricing</h1></a>
-            <a href = "/#"><h1 className = "navElement">Learn</h1></a>
-            <a href = "/#"><h1 className = "navElement">Log In</h1></a>
-            <a href = "/#"><h1 className = "navElement test">Cart {this.state.Price}$</h1></a>
+            <a href = "#"><h1 className = "navElement">Pricing</h1></a>
+            <a href = "#"><h1 className = "navElement">Learn</h1></a>
+            <a href = "#"><h1 className = "navElement">Log In</h1></a>
+            <a href = "#"><h1 className = "navElement test">Cart {this.state.Price}$</h1></a>
             </div>
           </div>
 
 
           <div className = "landingPage">
+                    {/*landing page description*/}
 
             <div className = "landingLHS">
               <h1 className = "landingHeader">The "Real" Fake <br/>Commerce Platform </h1>
@@ -67,10 +72,11 @@ class App extends React.Component {
           </div>
 
           <div className = "storePage">
-            
+                      {/* at the top of the storepage is the search bar*/}
+
             <input type = "text" placeholder = "Search Here" className = "searchbar" onChange= {event => {this.setState({searchWord : event.target.value})}}/>
             <ul className = "products">
-
+              {/* here we filter the array, if the search word is included in the product name, showcase the product, other wise show the name, img, price and rating*/}
               {this.state.products.filter(products => {if(this.state.searchWord == ""){
                 return products.name}
                 else if (products.name.toLowerCase().includes(this.state.searchWord.toLowerCase())){
